5b4f33bedb3125014ef975612251fc420187c767
Daniel Palmares <danielpalmares100@gmail.com>
2020-11-22 19:40:01 -0300

    resultsView.js, view.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,14 @@
-import * as model from './model';
-import recipeView from './view/recipeView';
-import searchView from './view/searchView';
+import * as model from './model.js';
+import recipeView from './view/recipeView.js';
+import searchView from './view/searchView.js';
+import resultsView from './view/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -25,6 +30,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -33,7 +40,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
