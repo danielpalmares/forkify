e1e323655e10d03bbe90096255d80f2ef3876993
Daniel Palmares <danielpalmares100@gmail.com>
2020-11-29 14:03:33 -0300

    Implementing bookmark

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './view/recipeView.js';
 import searchView from './view/searchView.js';
 import resultsView from './view/resultsView.js';
 import paginationView from './view/paginationView.js';
+import bookmarksView from './view/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -20,6 +21,7 @@ const controlRecipes = async function () {
 
     // 0 *-- Update results view to mark selected search result --*
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1 *-- Loading the recipe --*
     await model.loadRecipe(id);
@@ -73,10 +75,16 @@ const controlServings = function(newServings) {
 };
 
 const controlAddBookmark = function() {
+  // 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
   
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks)
+
 }
 
 const init = function () {
